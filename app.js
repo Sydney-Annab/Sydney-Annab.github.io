ec3a2ce03e03a32729613144ccb0552de0840dfe
Sydney-Annab <104046892+Sydney-Annab@users.noreply.github.com>
2023-02-17 23:41:38 -0500

    login

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-document.addEventListener("DOMContentLoaded", )
+document.addEventListener("DOMContentLoaded", () => {
+    const login_form = document.querySelector("#login_form");
+    const create_account_form = document.querySelector("#create_account_form");
+
+    document.querySelector("linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        login_form.classList.add("form-hidden");
+        create_account_form.classList.remove("form_hidden");
+    });
+
+    document.querySelector("linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        login_form.classList.add("form-hidden");
+        create_account_form.classList.remove("form_hidden");
+    });
