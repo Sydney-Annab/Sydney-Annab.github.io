fec824a9373b728f3a8f5a5ffea74ca1dafd5bd0
Sydney-Annab <104046892+Sydney-Annab@users.noreply.github.com>
2023-02-20 15:30:58 -0500

    login

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const login_form = document.querySelector("#login_form");
-    const create_account_form = document.querySelector("#create_account_form");
+    const LoginForm = document.querySelector("#login_form");
+    const CreateAccountForm = document.querySelector("#create_account_form");
 
     document.querySelector("linkCreateAccount").addEventListener("click", e => {
         e.preventDefault();
-        login_form.classList.add("form_hidden");
-        create_account_form.classList.remove("form_hidden");
+        LoginForm.classList.add("form_hidden");
+        CreateAccountForm.classList.remove("form_hidden");
     });
 
     document.querySelector("linkLogin").addEventListener("click", e => {
         e.preventDefault();
-        create_account_form.classList.add("form_hidden");
-        login_form.classList.remove("form_hidden");
+        CreateAccountForm.classList.add("form_hidden");
+        LoginForm.classList.remove("form_hidden");
     });
 });
