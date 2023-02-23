6c0d6cb772bef727a7fcb95d6ea3fff26855bafa
Sydney-Annab <104046892+Sydney-Annab@users.noreply.github.com>
2023-02-22 23:11:46 -0500

    post

--- dropdown.js
+++ dropdown.js
@@ -1,15 +1,15 @@
 var commonValues ={
-    Structural : ["Beams", "Columns"],
-    Enclosure : ["Interior Wall Finishings", "Exterior Wall Finishings"]
+    Structural: ["Beams", "Columns"],
+    Enclosure: ["Interior Wall Finishings", "Exterior Wall Finishings"]
 }
 
-function changeDropdownValue(){
+function changeDropdownValue(value){
     if(value.length == 0){
         document.getElementById("category").innerHTML = "<option></option>"
     }else{
         var commonOptions = "";
         for(categoryID in commonValues[value]){
-            commonOptions +="<option>" + commonValue[value][categoryID] + "</option>"
+            commonOptions +="<option>" + commonValues[value][categoryID] + "</option>"
         }
         document.getElementById("category").innerHTML
     }
